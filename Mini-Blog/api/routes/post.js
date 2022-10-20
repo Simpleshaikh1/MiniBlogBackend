3e5959e91552538b24ce72ffbd635f8a091ee653
festuspete <festuspeteragbo@gmail.comgit config --global user.name festuspetegit config --global user.email festuspeteragbo@gmail.com>
2022-10-20 18:59:54 +0100

    fresh post commit

--- Mini-Blog/api/routes/post.js
+++ Mini-Blog/api/routes/post.js
@@ -1,70 +1,33 @@
-const router = require('express').Router();
-const User = require("../models/user");
-const Post = require('../models/news')
+// const { Router } = require('express');
+const express = require ('express');
+// const newsController = express('../controller/newsController');
 
+const {
+    getAllNews,
+    getAllSingleUserNews,
+    createNews,
+    updateNews,
+    deleteNews,
+    getSingleNews
+} = require('../controller/newsController')
 
+const router = express.Router();
 
-//CREATE NEW POST
-router.post('/', async (req, res)=>{
-    const newPost = new Post(req.body);
-    try {
-        const savedPost = await newPost.save()
-        res.status(200).json(savedPost)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-    });
+router
+    .route('/')
+    .get(getAllNews)
+    .post(createNews);
 
-//UPDATE POST
+router
+.route('/getAllSingleUserNews')
+.get(getAllSingleUserNews)
 
-router.put('/:id', async (req, res)=>{
-    try {
-        const post  = await Post.findById(req.params.id);
-        if (post.username === req.body.username)
-            try {
-                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
-                    $set:req.body
-                }, {new: true});
-                res.status(200).json(updatedPost)
-            } catch (error) {
-                res.status(500).json(error)
-            }else{
-                res.status(401).json('you can your post only')
-            }
-    } catch (error) {
-        res.status(500).json(error)
-    }
-});
+router
+    .route('/:id')
+    .get(getSingleNews)
+    .patch(updateNews)
+    .delete(deleteNews)
 
-
-//DELETE POST
-router.delete('/:id', async (req, res)=>{
-    try {
-        const post  = await Post.findById(req.params.id);
-        if (post.username === req.body.username)
-            try {
-                await post.delete();
-                res.status(200).json('post deleted')
-            } catch (error) {
-                res.status(500).json(error)
-            }else{
-                res.status(401).json('you can delete your post only')
-            }
-    } catch (error) {
-        res.status(500).json(error)
-    }
-});
-
-
-//GET POST
-router.get("/:id", async (req, res) =>{
-    try {
-        const post = await Post.findById(req.params.id)
-       
-        res.status(200).json(post)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
-
+    
+    
 module.exports = router;
