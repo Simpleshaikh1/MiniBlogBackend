85b3402e2709c4e5555e5d28aba4961322e31a2d
Toyyib Bolaji <58138876+Simpleshaikh1@users.noreply.github.com>
2022-10-20 22:45:01 +0100

    Update post.js

--- Mini-Blog/api/routes/post.js
+++ Mini-Blog/api/routes/post.js
@@ -1,33 +1,27 @@
 // const { Router } = require('express');
-const express = require ('express');
+const express = require("express");
+const authMiddleware = require("../middleware/auth");
 // const newsController = express('../controller/newsController');
-
-const {
-    getAllNews,
-    getAllSingleUserNews,
-    createNews,
-    updateNews,
-    deleteNews,
-    getSingleNews
-} = require('../controller/newsController')
-
 const router = express.Router();
 
-router
-    .route('/')
-    .get(getAllNews)
-    .post(createNews);
+const {
+  getAllNews,
+  getAllSingleUserNews,
+  createNews,
+  updateNews,
+  deleteNews,
+  getSingleNews,
+} = require("../controller/newsController");
+
+
+router.route("/").get(getAllNews).post(authMiddleware, createNews);
+
+router.route("/getAllSingleUserNews").get(getAllSingleUserNews);
 
 router
-.route('/getAllSingleUserNews')
-.get(getAllSingleUserNews)
+  .route("/:id")
+  .get(getSingleNews)
+  .patch(authMiddleware, updateNews)
+  .delete(authMiddleware, deleteNews);
 
-router
-    .route('/:id')
-    .get(getSingleNews)
-    .patch(updateNews)
-    .delete(deleteNews)
-
-    
-    
 module.exports = router;
