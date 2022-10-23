3e24d5f754883409d4ac4c81e54acc46ca8abe8b
Toyyib Bolaji <58138876+Simpleshaikh1@users.noreply.github.com>
2022-10-23 17:53:19 +0100

    Update newsController.js

--- Mini-Blog/api/controller/newsController.js
+++ Mini-Blog/api/controller/newsController.js
@@ -1,107 +1,95 @@
-const News = require('../models/news')
+const News = require("../models/news");
 
 const getAllNews = async (req, res) => {
-    try {
-        const news = await News.find({});
+  try {
+    const news = await News.find({});
 
-        res.status(200).json({
-            status: 'Success',
-            count: news.length,
-            data: {
-                news
-            },
-        })
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
+    res.status(200).json({
+      status: "Success",
+      count: news.length,
+      data: {
+        news,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const createNews = async (req, res) =>{
-    try {
-        const news = await News.create(req.body)
-        res.status(200).json({
-            status: 'Success',
-            data: {
-                news
-            }
-        })
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
-
+const createNews = async (req, res) => {
+  const { title, body, id } = req.body;
+  try {
+    const news = await await News.create({ title, body, authorSchema_id: id });
+    res.status(200).json({
+      status: "Success",
+      data: {
+        news,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+// issue
 const getAllSingleUserNews = async (req, res) => {
-    try {
-        const news = await News.find({ user: req.user.userId })
-        res.status(200).json({ news, count: news.length})
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
-
+  try {
+    const news = await News.findById(req.params.id);
+    res.status(200).json({ news, count: news.length });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const updateNews = async (req, res) => {
-    try {
-        const news = await News.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidator: true,
-        })
-        res.status(200).json({
-            status: 'Success',
-            data: {
-                news
-            }
-        })
-        
-    } catch (error) {
-        console.error(error)
-        
-    }
-}
+  try {
+    const news = await News.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidator: true,
+    });
+    res.status(200).json({
+      status: "Success",
+      data: {
+        news,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const deleteNews = async (req, res) => {
+  try {
+    await News.findByIdAndDelete(req.params.id);
 
-    try {
-        await News.findByIdAndDelete(req.params.id);
-    
-        res.status(200).json({
-            status: 'Success',
-            message: 'News deleted successfully'
-        })
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
+    res.status(200).json({
+      status: "Success",
+      message: "News deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const getSingleNews  = async (req, res) => {
-    try {
-        const news = await News.findById(req.params.id);
-    
-        res.status(200).json({
-            status: 'Success',
-            data: {
-                news
-            }
-        })
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
+const getSingleNews = async (req, res) => {
+  try {
+    const news = await News.findById(req.params.id);
 
+    res.status(200).json({
+      status: "Success",
+      data: {
+        news,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    getAllNews,
-    getAllSingleUserNews,
-    createNews,
-    updateNews,
-    deleteNews,
-    getSingleNews
-}
+  getAllNews,
+  getAllSingleUserNews,
+  createNews,
+  updateNews,
+  deleteNews,
+  getSingleNews,
+};
