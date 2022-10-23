506fa115fc7845a65c4e0149fe6d8748621f683b
Toyyib Bolaji <58138876+Simpleshaikh1@users.noreply.github.com>
2022-10-23 17:51:51 +0100

    Update userController.js

--- Mini-Blog/api/controller/userController.js
+++ Mini-Blog/api/controller/userController.js
@@ -1,84 +1,75 @@
 const Author = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-     try {
-          const authorTaken = await validateEmail(req.body.email);
-          if (authorTaken) {
-               return res.status(400).json({
-                    message: "Email already exists",
-               });
-          }
+  try {
+    const authorTaken = await validateEmail(req.body.email);
+    if (authorTaken) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
 
-          const newAuthor = new Author({
-               ...req.body,
-          });
+    const newAuthor = new Author({
+      ...req.body,
+    });
 
-          await newAuthor.save();
-          return res.status(201).json({
-               message: "Account successfully created",
-               // success: true,
-               data: newAuthor,
-          });
-     } catch (err) {
-          return res.status(500).json({
-               message: err.message,
-               success: false,
-          });
-     }
+    await newAuthor.save();
+    return res.status(201).json({
+      message: "Account successfully created",
+      data: newAuthor,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+      success: false,
+    });
+  }
 };
 
-
-const login = async(req,res)=>{
-    try {
-        let { email, password } = req.body;
-        const author = await Author.findOne({ email });
-        if(!author) {
-            res.status(404).json({
-                message: "Failed login attempt"
-            })
-        }
-        let isMatch = await user.matchPasswords(password);
-        if(isMatch) {
-            let profile = {
-                email: author.email,
-                name: author.name,
-            };
-            let result = {
-                author: profile,
-                token:sendToken(author,res)
-            };
-            return res.status(200).json({
-                ...result,
-                message: "Login success",
-            });
-        } else {
-            return res.status(403).json({
-                message: "Failed login attempt"
-            })
-        }
-    } catch(err) {
-        return res.status(500).json({
-            message: err.message,
-        })
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const author = await Author.findOne({ email });
+    if (!author) {
+      res.status(403).json({
+        message: "Failed login attempt",
+      });
     }
-    
-}
+    let isMatch = await author.matchPasswords(password);
+    if (isMatch) {
 
+     let token = author.getSignedJwtToken(author._id)
+
+      
+      res.status(200).json({
+        message: "Login success",
+        status: true,
+        data: token
+      });
+    } else {
+      res.status(403).json({
+        message: "Failed login attempt",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
 const validateEmail = async (email) => {
-     let author = await Author.findOne({ email });
-     if (author) {
-          return true;
-     } else {
-          return false;
-     }
+  let author = await Author.findOne({ email });
+  if (author) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const sendToken = (author,res) => {
-     const token = author.getSignedJwtToken();
-     res.json({ sucess: true, token });
-};
+
 module.exports = {
-     register,
-     login,
+  register,
+  login,
 };
