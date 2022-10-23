bd27677f2cf1a6ccf56d0c2b1582fa61f78a8283
Toyyib Bolaji <58138876+Simpleshaikh1@users.noreply.github.com>
2022-10-23 17:59:45 +0100

    Update index.js

--- Mini-Blog/api/index.js
+++ Mini-Blog/api/index.js
@@ -5,18 +5,21 @@ const app = express();
 const connectDB = require("./db/connect");
 
 const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/post");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoute);
+app.get("/", (req, res)=> {
+  res.json({msg:"api is working fine"})
+})
+// app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
 const PORT = process.env.PORT || 5000;
+
 
 const start = async () => {
   try {
