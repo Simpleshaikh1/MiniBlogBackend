36c89fe69db87131fbd67f7578f9217fc0898873
Toyyib Bolaji <58138876+Simpleshaikh1@users.noreply.github.com>
2022-10-25 15:31:39 +0100

    Update profileController.js

--- Mini-Blog/api/controller/profileController.js
+++ Mini-Blog/api/controller/profileController.js
@@ -1,33 +1,39 @@
 const Author = require("../models/user");
+const cloudinary = require("cloudinary").v2;
 
 const updateOne = async (req, res) => {
-  try {
-    req.author._id === req.params;
-    const author = await Author.findOne(req.author._id);
-    if (!author) {
-      res.status(401).json({
-        msg: "Unauthorized Access to use route",
-      });
-    }
-    await Author.findByIdAndUpdate(req.author._id, req.body);
-
-    return res.status(201).json({
-      message: "Author successfully updated",
-      success: true,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-      success: false,
-    });
+  const { id } = req.params;
+  if (!id) {
+    res.status(404).json({ msg: `Author not found` });
   }
+
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "file-upload",
+    }
+  );
+
+  const newUpate = {
+    ...req.body,
+    image: result.secure_url,
+  };
+
+  const author = await Author.findByIdAndUpdate({ _id: id }, newUpate, {
+    new: true,
+    runValidators: true,
+  }).select("-password")
+
+  res.json({ author, ImageURL: result.secure_url });
 };
 
+
 const getAuthor = async (req, res) => {
-  
   try {
-    let id = req.params.id
-    const author = await Author.findById(id).select("-password");
+    let id = req.params.id;
+    const author = await Author.findById(id)
+      .select("-password").select("-isTrash");
     if (author) {
       return res.status(200).json(author);
     }
