bd294b87da47af967cd23511661e04da5766fcfc
Toyyib Bolaji <58138876+Simpleshaikh1@users.noreply.github.com>
2022-10-26 03:35:52 +0100

    Update users.js

--- Mini-Blog/api/routes/users.js
+++ Mini-Blog/api/routes/users.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middleware/auth");
 const { register, login } = require("../controller/userController");
-const { updateOne, getAuthor } = require("../controller/profileController");
+
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router
-  .route("/editor/:id")
-  .patch(authMiddleware, updateOne)
-  .get(authMiddleware, getAuthor);
+
 
 module.exports = router;
